241b8f370fd7bcdd8ea7f5ac340f0be66de5aa0b
David Pastor Vicente <david.p.vicente@stormharvester.com>
2021-12-11 21:31:31 +0000

    Add day 9

--- 2021/day9.py
+++ 2021/day9.py
@@ -13,18 +13,18 @@ heights = np.array(list(map(lambda x: [int(i) for i in x], text.split('\n'))))
 def get_adjacents(values, x, y):
     adjs = []
     if x > 0:
-        adjs.append(values[x-1][y])
+        adjs.append((x-1, y))
     if x < len(values)-1:
-        adjs.append(values[x+1][y])
+        adjs.append((x+1, y))
     if y > 0:
-        adjs.append(values[x][y-1])
+        adjs.append((x, y-1))
     if y < len(values[0])-1:
-        adjs.append(values[x][y+1])
+        adjs.append((x, y+1))
     return adjs
 
 
 def check_low(values, x, y):
-    if all(adj > values[x][y] for adj in get_adjacents(values, x, y)):
+    if all(values[adj[0]][adj[1]] > values[x][y] for adj in get_adjacents(values, x, y)):
         return values[x][y] + 1
     return 0
 
@@ -35,3 +35,26 @@ for i in range(len(heights)):
         num += check_low(heights, i, j)
 
 print(num)
+
+
+# Second problem
+
+def get_basin(values, x, y, basin):
+    adjs = get_adjacents(values, x, y)
+    basin.add((x, y))
+
+    for adj in adjs:
+        if values[adj[0]][adj[1]] != 9 and values[adj[0]][adj[1]] > values[x][y]:
+            get_basin(values, adj[0], adj[1], basin)
+
+    return basin
+
+
+basins = []
+for i in range(len(heights)):
+    for j in range(len(heights[0])):
+        if check_low(heights, i, j):
+            basins.append(get_basin(heights, i, j, set()))
+
+basins.sort(key=len)
+print(len(basins[-1])*len(basins[-2])*len(basins[-3]))
